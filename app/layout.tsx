da8cea11a4c08654fe9c0182211ce9edf2cc8700
rezine9612-collab <rezine9612@gmail.com>
2026-02-01 21:28:35 +0900

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="npHome-scope npReport-scope">
       <body>{children}</body>
     </html>
   );
