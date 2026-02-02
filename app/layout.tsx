7d1e952546d08a0c196c74200a06a64b2b3d7c89
rezine9612-collab <rezine9612@gmail.com>
2026-02-02 20:32:40 +0900

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,12 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=arrow_right_alt"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
